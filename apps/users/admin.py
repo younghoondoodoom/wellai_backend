ef339740b5e8522d485e189de7946825eb3ffb44
Martina Lee <martinalee0528@gmail.com>
2022-02-13 15:54:59 +0900

    feat: userinfo, useroption admin 추가

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from .models import User
+from .models import User, UserInfo, UserOption
 
 
 # Register your models here.
@@ -37,4 +37,67 @@ class MyUserAdmin(admin.ModelAdmin):
     ordering = ["date_joined"]
 
 
+class MyUserInfoAdmin(admin.ModelAdmin):
+    model = UserInfo
+    list_display = [
+        "user_id",
+        "exercise_total",
+        "calories_total",
+        "exercise_date",
+        "exercise_day",
+        "modified_at",
+    ]
+
+    fields = (
+        "user_id",
+        "exercise_total",
+        "calories_total",
+        "exercise_date",
+        "exercise_day",
+    )
+    search_fields = ["user_id"]
+    list_filter = ["exercise_date", "exercise_day"]
+    ordering = ["user_id"]
+
+
+class MyUserOptionAdmin(admin.ModelAdmin):
+    model = UserOption
+    list_display = [
+        "user_id",
+        "gender",
+        "height",
+        "weight",
+        "stand",
+        "sit",
+        "balance",
+        "core",
+        "leg",
+        "back",
+        "modified_at",
+    ]
+    fieldsets = (
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "user_id",
+                    "gender",
+                    "height",
+                    "weight",
+                )
+            },
+        ),
+        (
+            _("favorite exercise"),
+            {"fields": ("stand", "sit", "balance", "core", "leg", "back")},
+        ),
+    )
+
+    search_fields = ["user_id"]
+    list_filter = ["gender"]
+    ordering = ["user_id"]
+
+
 admin.site.register(User, MyUserAdmin)
+admin.site.register(UserInfo, MyUserInfoAdmin)
+admin.site.register(UserOption, MyUserOptionAdmin)
