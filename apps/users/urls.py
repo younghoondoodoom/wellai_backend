b81e80b786b99445c19784f251e5285b15247096
Martina Lee <martinalee0528@gmail.com>
2022-02-19 22:36:38 +0900

    feat: 토큰 유효 체크 api 추가 #5

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView,
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from .views import UserCheckView, UserDetailView, UserOptionUpdateView, UserRegisterView
@@ -11,6 +12,7 @@ urlpatterns = [
     path("check/", UserCheckView.as_view(), name="user-check"),
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("logout/", TokenBlacklistView.as_view(), name="logout"),
     path("detail/", UserDetailView.as_view(), name="user-detail"),
