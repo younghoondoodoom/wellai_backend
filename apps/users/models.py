35424edd0f2cc8a9a037996b098c5520be15b647
Martina Lee <martinalee0528@gmail.com>
2022-02-26 17:04:47 +0900

    feat: UserOption 모델 구현 #11
    - User 가입시 선택 정보를 가진 모델 구현

--- apps/users/models.py
+++ apps/users/models.py
@@ -87,3 +87,36 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampModel, DeleteModel):
 
     def __str__(self):
         return self.email
+
+
+class UserOption(TimeStampModel, models.Model):
+    GENDER_CHOICES = [
+        (None, ""),
+        ("F", "여"),
+        ("M", "남"),
+    ]
+    user_id = models.OneToOneField(
+        User,
+        related_name="option",
+        on_delete=models.CASCADE,
+        db_column="email",
+    )
+    gender = models.CharField(
+        blank=True,
+        null=True,
+        max_length=1,
+        choices=GENDER_CHOICES,
+        default=None,
+        verbose_name="성별",
+    )
+    height = models.PositiveSmallIntegerField(default=0, verbose_name="키")
+    weight = models.PositiveSmallIntegerField(default=0, verbose_name="몸무게")
+    is_stand = models.BooleanField(default=False, verbose_name="서서")
+    is_sit = models.BooleanField(default=False, verbose_name="앉아서")
+    is_balance = models.BooleanField(default=False, verbose_name="밸런스")
+    is_core = models.BooleanField(default=False, verbose_name="코어")
+    is_leg = models.BooleanField(default=False, verbose_name="다리")
+    is_back = models.BooleanField(default=False, verbose_name="등")
+
+    def __str__(self):
+        return f"{self.user_id}"
