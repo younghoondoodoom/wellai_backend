8ce8d42c9184044c9d29d0cec094d9acf867c8dd
Martina Lee <martinalee0528@gmail.com>
2022-02-27 01:04:59 +0900

    feat: 로그아웃 api 추가 #20

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenObtainPairView
 
 urlpatterns = [
     path("login", TokenObtainPairView.as_view(), name="login"),
+    path("logout", TokenBlacklistView.as_view(), name="logout"),
 ]
