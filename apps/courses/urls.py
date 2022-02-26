d4c8485b50a449455b1c6cd7a045877536b020c2
ChoiYoungHoon <dudgns2865@naver.com>
2022-02-26 19:36:38 +0900

    feat: course bookmark delete api 구축 #18

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import BookMarkListCreateView
+from .views import BookMarkDeleteView, BookMarkListCreateView
 
 urlpatterns = [
     path("bookmark", BookMarkListCreateView.as_view(), name="course-bookmark"),
+    path(
+        "bookmark/<int:pk>", BookMarkDeleteView.as_view(), name="course-bookmark-delete"
+    ),
 ]
