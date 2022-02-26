7303122c2f458b31ca4f6267ff644f95786f4436
ChoiYoungHoon <dudgns2865@naver.com>
2022-02-26 19:44:04 +0900

    fix: git commit issue 원상복귀

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,10 +1,3 @@
 from django.urls import path
 
-from .views import BookMarkDeleteView, BookMarkListCreateView
-
-urlpatterns = [
-    path("bookmark", BookMarkListCreateView.as_view(), name="course-bookmark"),
-    path(
-        "bookmark/<int:pk>", BookMarkDeleteView.as_view(), name="course-bookmark-delete"
-    ),
-]
+urlpatterns = []
