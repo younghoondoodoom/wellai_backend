f1fea0d1744b83d68c8006cf339c88f057faf9ac
Martina Lee <martinalee0528@gmail.com>
2022-02-27 18:48:01 +0900

    feat: 회원가입 기본정보 체크 View 추가 #16

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import UserRegisterCheckView
+
+urlpatterns = [
+    path("check", UserRegisterCheckView.as_view(), name="user-check"),
+]
