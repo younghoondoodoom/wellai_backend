3d3c03041b34e414f618b60afce4020b31540963
ChoiYoungHoon <dudgns2865@naver.com>
2022-02-28 19:28:26 +0900

    fix: url 명사형으로 수정 #19

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import CourseRecommendView
 
 urlpatterns = [
-    path("recommend", CourseRecommendView.as_view(), name="course-recommend"),
+    path("recommendation", CourseRecommendView.as_view(), name="course-recommend"),
 ]
