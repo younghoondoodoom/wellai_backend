3fe7120a898e6f625ebc9695819b0f984bb5ff23
Martina Lee <martinalee0528@gmail.com>
2022-03-02 02:17:09 +0900

    feat: 유저 운동 기록 조회 시리얼라이저 추가 #26
    - UserRecordSummarySerializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,7 @@
 from django.core.validators import EmailValidator
 from rest_framework import serializers
 
+from .models import User, UserDailyRecord, UserOption
 from .validators import PasswordValidator
 
 
@@ -12,3 +13,35 @@ class UserRegisterCheckSerializer(serializers.Serializer):
     confirm_password = serializers.CharField(
         max_length=128, min_length=8, validators=[PasswordValidator()]
     )
+
+
+class UserDailyRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDailyRecord
+        fields = "__all__"
+
+
+class UserRecordSummarySerializer(serializers.ModelSerializer):
+    month_exercise_time = serializers.SerializerMethodField()
+    month_calories = serializers.SerializerMethodField()
+    records = UserDailyRecordSerializer(source="daily_record", many=True)
+
+    def get_month_exercise_time(self, obj):
+        time = list(obj.daily_record.aggregate(Sum("exercise_duration")).values())[0]
+        return int(time)
+
+    def get_month_calories(self, obj):
+        cals = list(obj.daily_record.aggregate(Sum("calories_total")).values())[0]
+        return cals
+
+    class Meta:
+        model = User
+        read_only_fields = ("nickname",)
+        fields = (
+            "id",
+            "email",
+            "nickname",
+            "month_exercise_time",
+            "month_calories",
+            "records",
+        )
