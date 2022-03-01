3c31cfeca96cb48a2e8612700ccd18a317331ccd
Martina Lee <martinalee0528@gmail.com>
2022-03-02 02:16:14 +0900

    feat: 회원 운동 기록 조회 기능 추가 #26
    - UserRecordListView 추가

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.db.models import Prefetch, Q
+from django.utils import timezone
+from rest_framework import generics, mixins, permissions, status
 
-# Create your views here.
+from apps.cores.permissions import IsOwner
+
+from .models import User, UserDailyRecord
+from .serializers import UserRecordSummarySerializer
+
+
+class UserRecordListView(generics.ListAPIView):
+    serializer_class = UserRecordSummarySerializer
+    permission_classes = [IsOwner]
+    current_month = timezone.now().strftime("%m")
+    current_year = timezone.now().strftime("%Y")
+
+    def get_queryset(self):
+        queryset = User.objects.filter(id=self.request.user.id).prefetch_related(
+            Prefetch(
+                "daily_record",
+                queryset=UserDailyRecord.objects.filter(
+                    exercise_date__year=self.current_year,
+                    exercise_date__month=self.current_month,
+                ),
+            )
+        )
+        return queryset
