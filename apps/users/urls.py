bb93f9ce3f38bd43894ad89e7dae8281bd5c1d79
Martina Lee <martinalee0528@gmail.com>
2022-03-01 19:12:52 +0900

    feat: 유저 추가정보 수정 기능 추가 #25
    - useroptionupdateview 추가

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,9 +6,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from .views import UserOptionUpdateView
+
 urlpatterns = [
     path("login", TokenObtainPairView.as_view(), name="login"),
     path("logout", TokenBlacklistView.as_view(), name="logout"),
     path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
     path("token/verify", TokenVerifyView.as_view(), name="token-verify"),
+    path("option", UserOptionUpdateView.as_view(), name="user-option-update"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.cores.permissions import IsOwner
+
+from .models import User, UserOption
+from .serializers import UserOptionSerializer, UserRegisterSerializer
+
+
+class UserOptionUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwner]
+    serializer_class = UserOptionSerializer
+    queryset = UserOption.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = User.objects.get(id=request.user.id)
+        serializer = UserRegisterSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        partial = True
+        instance = UserOption.objects.get_or_create(user_id=request.user)[0]
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
