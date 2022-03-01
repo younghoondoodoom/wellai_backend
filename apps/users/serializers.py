8401a2245d83e5e3c2a56d6add5bb118997dbc5f
Martina Lee <martinalee0528@gmail.com>
2022-03-01 19:14:05 +0900

    feat: 유저 추가정보 시리얼라이저 수정 # 25
    - 추가정보 수정(업데이트)에 맞게 시리얼라이저 수정

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,7 @@
 from django.core.validators import EmailValidator
 from rest_framework import serializers
 
+from .models import User, UserOption
 from .validators import PasswordValidator
 
 
@@ -12,3 +13,38 @@ class UserRegisterCheckSerializer(serializers.Serializer):
     confirm_password = serializers.CharField(
         max_length=128, min_length=8, validators=[PasswordValidator()]
     )
+
+
+class UserOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserOption
+        fields = (
+            "modified_at",
+            "created_at",
+            "gender",
+            "height",
+            "weight",
+            "is_stand",
+            "is_sit",
+            "is_balance",
+            "is_core",
+            "is_leg",
+            "is_back",
+        )
+        excludes = (
+            "id",
+            "user_id",
+        )
+        read_only_fields = ("modified_at", "created_at")
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    options = UserOptionSerializer(source="option")
+    password = serializers.CharField(
+        max_length=128, validators=[PasswordValidator()], write_only=True
+    )
+
+    class Meta:
+        model = User
+        read_only_fields = ("nickname",)
+        fields = ("email", "nickname", "password", "options")
