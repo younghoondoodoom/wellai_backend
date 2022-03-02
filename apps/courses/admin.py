2bcfc876bbd38b35ea83a6f2428b6f938903ce40
ChoiYoungHoon <dudgns2865@naver.com>
2022-03-02 22:38:57 +0900

    fix: merge issue 해결

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import BookMark, Course, CourseReview, Exercise, Tag
+
 # Register your models here.
+
+admin.site.register(Exercise)
+admin.site.register(Course)
+admin.site.register(CourseReview)
+admin.site.register(Tag)
+admin.site.register(BookMark)
