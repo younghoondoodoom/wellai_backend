91c3247a1d823a55349bdc116234453d697210cd
Martina Lee <martinalee0528@gmail.com>
2022-03-02 20:01:26 +0900

    fix: date exception 삭제

--- apps/users/exceptions.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import APIException
-
-
-class InvalidDateException(APIException):
-    status_code = 400
-    default_detail = {"message": ["정확한 연도, 월을 입력해 주세요."]}
-    default_code = _("쿼리 인풋값 에러")
