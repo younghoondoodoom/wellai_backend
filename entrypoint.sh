5a69b5dc786dad41b16ab1bc6b93138fbc37029c
ChoiYoungHoon <dudgns2865@naver.com>
2022-03-03 19:33:43 +0900

    feat: docker 환경 컨테이너 구축 #6

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,3 @@ echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py shell < entryadmin.py
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
