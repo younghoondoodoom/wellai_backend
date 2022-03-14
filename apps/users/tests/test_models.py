4c28bc30f558cfd51786cc52ed20e6903c6cc16e
Martina Lee <martinalee0528@gmail.com>
2022-03-14 23:10:22 +0900

    feat: user model 테스트 코드 추가 #46
    - pytest-cov 의존성 추가

--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -1,13 +1,66 @@
-import factory
 import pytest
-from apps.users.models import User, UserInfo, UserOption
-from django.urls import reverse
-from pytest_factoryboy import register
+from apps.users.models import User, UserDailyRecord, UserOption
+from django.utils import timezone
+from faker import Faker
 
-from .factories import AdminUserFactory, UserFactory, UserInfoFactory, UserOptionFactory
+fake = Faker()
 
 
-@pytest.mark.django_db
-def test_create_user_success():
-    UserFactory.create()
-    assert User.objects.count() == 1
+@pytest.mark.django_db(transaction=True)
+class TestUserModel:
+    @pytest.fixture()
+    def setup_user(self):
+        user = User.objects.create_user(
+            email=fake.email(),
+            password=fake.password(length=8),
+        )
+        return user
+
+    def test_create_user_success(self, setup_user):
+        user = setup_user
+        assert str(user) == user.email
+
+    def test_create_user_email_empty_fail(self):
+        with pytest.raises(ValueError):
+            User.objects.create_user(email="", password=fake.password(length=8))
+
+    def test_create_user_password_empty_fail(self):
+        with pytest.raises(ValueError):
+            User.objects.create_user(email=fake.email(), password="")
+
+    def test_create_superuser_success(self):
+        User.objects.create_superuser(email=fake.email(), password="1234")
+
+    def test_create_superuser_email_empty_fail(self):
+        with pytest.raises(ValueError):
+            User.objects.create_superuser(email="", password="1234")
+
+    def test_create_superuser_is_staff_fail(self):
+        with pytest.raises(ValueError):
+            User.objects.create_superuser(
+                email=fake.email(), password="1234", is_staff=False
+            )
+
+    def test_create_useroption_success(self, setup_user):
+        user = setup_user
+        option = UserOption.objects.create(
+            user_id=user,
+            gender="F",
+            height=0,
+            weight=0,
+            is_stand=False,
+            is_sit=False,
+            is_balance=True,
+            is_core=False,
+            is_arm=True,
+            is_recline=False,
+        )
+        assert str(option) == option.user_id.email
+
+    def test_create_user_daily_record_success(self, setup_user):
+        user = setup_user
+        date = timezone.now()
+        record = UserDailyRecord.objects.create(
+            user_id=user, exercise_date=date, exercise_duration=3600
+        )
+        assert str(record) == f"{user.email} - {date}"
